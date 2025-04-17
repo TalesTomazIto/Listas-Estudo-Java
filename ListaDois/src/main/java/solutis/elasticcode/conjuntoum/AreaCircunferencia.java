85ef19e5dd2501c5b176e76ee4f9494db622f8a3
TalesTomazIto <tales.ito@sptech.school>
2025-04-17 09:10:44 -0300

    Exclusao da lista 2 do main

--- ListaDois/src/main/java/solutis/elasticcode/conjuntoum/AreaCircunferencia.java
+++
@@ -1,8 +0,0 @@
-package solutis.elasticcode.conjuntoum;
-
-public class AreaCircunferencia {
-    public static void main(String[] args) {
-        int raioCirc = 5;
-        System.out.println("A área do círculo é: " + Math.round(3.14 * raioCirc * 2));
-    }
-}
